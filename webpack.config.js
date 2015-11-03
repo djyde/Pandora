b434527b10a270fb056682ff7494f36a8e30f628
Randy <randypriv@gmail.com>
2015-11-04 00:27:37 +0800

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: './app/app.js',
+  output: {
+    path: './build',
+    filename: 'app.bundle.js'
+  },
+  modules: {
+    loader: [
+      {
+        
+      }
+    ]
+  }
+}
